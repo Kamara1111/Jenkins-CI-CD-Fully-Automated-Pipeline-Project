7a95fba502c0d45849fb678ad822550bb8a5e035
Kamara1111 <147885116+Kamara1111@users.noreply.github.com>
2025-02-20 02:57:29 -0700

    Update nexus-setup.sh

--- nexus-setup.sh
+++ nexus-setup.sh
@@ -1,59 +1,49 @@
 #!/bin/bash
-# Hardware requirements: AWS Linux 2 with mimum t2.medium type instance & port 8081 should be allowed on the security groups
+# Update system packages
+dnf update -y
 
+# Install Java (Nexus requires Java)
+dnf install java-17-amazon-corretto -y
 
-# node-exporter installations
-sudo useradd --no-create-home node_exporter
-
-wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
-sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
-rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
-
-# setup the node-exporter dependencies
-sudo yum install git -y
-sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
-sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable node-exporter
-sudo systemctl start node-exporter
-sudo systemctl status node-exporter
-
-
-# Nexus setup and installations
-yum install java-1.8.0-openjdk.x86_64 wget -y   
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
-cd /tmp/nexus/
-NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
-wget $NEXUSURL -O nexus.tar.gz
-EXTOUT=`tar xzvf nexus.tar.gz`
-NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
-rm -rf /tmp/nexus/nexus.tar.gz
-rsync -avzh /tmp/nexus/ /opt/nexus/
+# Create Nexus User
 useradd nexus
-chown -R nexus.nexus /opt/nexus 
-cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
+echo "nexus ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-EOT
+# Download and Extract Nexus Repository Manager
+cd /opt
+wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz -O nexus.tar.gz
+tar -xvzf nexus.tar.gz
+rm -f nexus.tar.gz
+mv nexus-3* nexus
 
-echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
+# Set Ownership and Permissions
+chown -R nexus:nexus /opt/nexus
+chown -R nexus:nexus /opt/sonatype-work
+
+# Configure Nexus to Run as Nexus User
+echo 'run_as_user="nexus"' > /opt/nexus/bin/nexus.rc
+
+# Enable Nexus Service on Startup
+cat <<EOF > /etc/systemd/system/nexus.service
+[Unit]
+Description=nexus service
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=65536
+User=nexus
+Group=nexus
+ExecStart=/opt/nexus/bin/nexus start
+ExecStop=/opt/nexus/bin/nexus stop
+User=nexus
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload and Start Nexus Service
 systemctl daemon-reload
-systemctl start nexus
 systemctl enable nexus
-
+systemctl start nexus
